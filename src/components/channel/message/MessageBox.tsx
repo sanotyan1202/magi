a8ec0ba81ee7336d69d82d9b0439ce92b8bc2797
sano <taikisano1202@gmail.com>
2024-10-31 16:52:59 +0900

    マークダウン対応（暫定）

--- src/components/channel/message/MessageBox.tsx
+++ src/components/channel/message/MessageBox.tsx
@@ -1,4 +1,6 @@
 import { Message } from "@prisma/client"
+import ReactMarkdown from 'react-markdown'
+import styles from '@/styles/MessageBox.module.css'
 
 type Props = {
   message: Message
@@ -6,10 +8,12 @@ type Props = {
 
 export default function MessageBox({ message }: Props) {
   return (
-    <div key={message.id} className="mt-4">
-      <div className="p-3 mb-2 bg-white rounded-lg shadow">
+    <div className="mt-4">
+      <div className={`p-3 mb-2 bg-white rounded-lg shadow ${styles.markdownContent}`}>
         <div className="font-bold">{message.name}</div>
-        <div className="text-sm text-gray-600">{message.content}</div>
+        <div className="text-sm text-gray-600">
+          <ReactMarkdown>{message.content}</ReactMarkdown>
+        </div>
       </div>
     </div>
   )
